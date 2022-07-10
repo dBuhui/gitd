473de6139282dc7f9bcd4ab8de59d46080e129c2
aluka <alukahaha@163.com>
2022-07-10 11:49:21 +0800

    first commit

---
+++ src/main/java/com/atguigu/gitexc.java
@@ -0,0 +1,11 @@
+package com.atguigu;
+
+/**
+ * @author akk
+ * @version 1.0
+ */
+public class gitexc {
+    public static void main(String[] args) {
+        System.out.println("hello,guigu");
+    }
+}
